c848bc3c6fe933b933bd1af914bf9cd4bc76c472
Gashaw M. Goshu <56516076+gashawmg@users.noreply.github.com>
2025-04-15 19:24:29 -0500

    Update water_solubility.py

--- water_solubility.py
+++ water_solubility.py
@@ -1,5 +1,5 @@
 # import the necessary libraries
-
+st.set_page_config(page_title='Aqueous Solubility Prediction App', layout='wide')
 import numpy as np
 import pandas as pd
 from matplotlib import image, pyplot as plt
@@ -17,7 +17,7 @@ from rdkit.ML.Descriptors import MoleculeDescriptors
 # streamlit ---------------------------------------------------------
 import streamlit as st
 # MUST be the first Streamlit command
-st.set_page_config(page_title='Aqueous Solubility Prediction App', layout='wide')
+
 from PIL import Image
 import base64
 import io
